93b68e9e6bdf78da5d3b820adb131a0b0550e341
Sriraghav Sira <sira50698@gmail.com>
2020-07-20 13:26:16 -0500

    added eureka server config

--- discovery-server/src/main/java/com/sira/app/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/sira/app/discoveryserver/DiscoveryServerApplication.java
@@ -1,9 +1,11 @@
 package com.sira.app.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
