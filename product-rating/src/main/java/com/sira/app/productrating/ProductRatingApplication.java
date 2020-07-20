e763f5fcf6210e5bf506df48ad3857cbfdf97f24
Sriraghav Sira <sira50698@gmail.com>
2020-07-20 13:55:25 -0500

    added discovery client config

--- product-rating/src/main/java/com/sira/app/productrating/ProductRatingApplication.java
+++ product-rating/src/main/java/com/sira/app/productrating/ProductRatingApplication.java
@@ -2,8 +2,10 @@ package com.sira.app.productrating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductRatingApplication {
 
 	public static void main(String[] args) {
